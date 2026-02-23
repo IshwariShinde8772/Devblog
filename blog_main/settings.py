8b2f5c2b24340dccedf9ee1c718a8e30b98611d7
Ishwari <Ishwarishinde2006@gmail.com>
2026-02-23 08:07:10 +0530

    Configure project for Render deployment (WhiteNoise, Procfile, build.sh)

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-n8^@1$ll9wyo)$h5h+5xq-a7a!vnn&@5(gi#!3*9#t@=nqc&al
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -46,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
